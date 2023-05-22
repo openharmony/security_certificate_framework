a44f202a61f324873d829052511dd1af80b73be9
huyiqi <huyiqi3@huawei.com>
2023-05-22 21:13:32 +0800

    add extension object napi code
    Signed-off-by: huyiqi <huyiqi3@huawei.com>

---
+++ frameworks/js/napi/certificate/inc/napi_cert_extension.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NAPI_CERT_EXTENSION_H
+#define NAPI_CERT_EXTENSION_H
+
+#include <string>
+
+#include "napi/native_api.h"
+#include "napi/native_common.h"
+
+#include "cf_api.h"
+
+namespace OHOS {
+namespace CertFramework {
+class NapiCertExtension {
+public:
+    explicit NapiCertExtension(CfObject *object);
+    ~NapiCertExtension();
+
+    static void DefineCertExtensionJsClass(napi_env env, napi_value exports);
+
+    CfObject *GetObject()
+    {
+        return object_;
+    }
+
+    static thread_local napi_ref classRef_;
+
+private:
+    CfObject *object_ = nullptr;
+};
+} // namespace CertFramework
+} // namespace OHOS
+
+#endif // NAPI_CERT_EXTENSION_H
---
+++ frameworks/js/napi/certificate/src/napi_cert_extension.cpp
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "napi_cert_extension.h"
+
+#include "napi/native_node_api.h"
+#include "napi/native_api.h"
+
+#include "cf_api.h"
+#include "cf_log.h"
+#include "cf_memory.h"
+#include "cf_param.h"
+#include "cf_result.h"
+
+#include "napi_cert_defines.h"
+#include "napi_cert_utils.h"
+#include "napi_common.h"
+#include "napi_object.h"
+
+namespace OHOS {
+namespace CertFramework {
+thread_local napi_ref NapiCertExtension::classRef_ = nullptr;
+
+struct CfExtensionAsyncContext {
+    AsyncCtx async = nullptr;
+
+    CfEncodingBlob *encodingBlob = nullptr;
+    CfObject *extsObj = nullptr;
+};
+using ExtsAsyncContext = CfExtensionAsyncContext *;
+
+NapiCertExtension::NapiCertExtension(CfObject *object)
+{
+    this->object_ = object;
+}
+
+NapiCertExtension::~NapiCertExtension()
+{
+    if (this->object_ != nullptr) {
+        this->object_->destroy(&this->object_);
+    }
+}
+
+static ExtsAsyncContext NewExtsAsyncContext(void)
+{
+    ExtsAsyncContext extsAsyncCtx = static_cast<ExtsAsyncContext>(CfMalloc(sizeof(CfExtensionAsyncContext)));
+    if (extsAsyncCtx == nullptr) {
+        CF_LOG_E("Failed to malloc extension async context");
+        return nullptr;
+    }
+
+    AsyncCtx asyncCtx = static_cast<AsyncCtx>(CfMalloc(sizeof(AsyncContext)));
+    if (asyncCtx == nullptr) {
+        CF_LOG_E("Failed to malloc async context");
+        CfFree(extsAsyncCtx);
+        return nullptr;
+    }
+
+    extsAsyncCtx->async = asyncCtx;
+    return extsAsyncCtx;
+}
+
+static void DeleteExtsAsyncContext(napi_env env, ExtsAsyncContext &context)
+{
+    if (context == nullptr) {
+        return;
+    }
+
+    if (context->async != nullptr) {
+        if (context->async->asyncWork != nullptr) {
+            napi_delete_async_work(env, context->async->asyncWork);
+            context->async->asyncWork = nullptr;
+        }
+
+        if (context->async->callback != nullptr) {
+            napi_delete_reference(env, context->async->callback);
+            context->async->callback = nullptr;
+        }
+    }
+    CfFree(context->async);
+
+    CfEncodingBlobDataFree(context->encodingBlob);
+    CfFree(context->encodingBlob);
+    context->encodingBlob = nullptr;
+
+    CfFree(context);
+}
+
+static napi_value ParseCreateExtsJSParams(napi_env env, napi_callback_info info, ExtsAsyncContext context)
+{
+    size_t argc = ARGS_SIZE_TWO;
+    napi_value argv[ARGS_SIZE_TWO] = { nullptr };
+    napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+    if (!CertCheckArgsCount(env, argc, ARGS_SIZE_TWO, false)) {
+        return nullptr;
+    }
+
+    if (!GetEncodingBlobFromValue(env, argv[PARAM0], &context->encodingBlob)) {
+        CF_LOG_E("get encoding blob from data failed!");
+        return nullptr;
+    }
+
+    context->async->asyncType = (argc == ARGS_SIZE_TWO) ? ASYNC_TYPE_CALLBACK : ASYNC_TYPE_PROMISE;
+    if (!GetCallbackAndPromise(env, context->async, argv[PARAM1])) {
+        return nullptr;
+    }
+
+    return NapiGetInt32(env, 0);
+}
+
+static napi_value CreateCertExtsJSInstance(napi_env env)
+{
+    napi_value constructor = nullptr;
+    napi_value instance = nullptr;
+    napi_get_reference_value(env, NapiCertExtension::classRef_, &constructor);
+    napi_new_instance(env, constructor, 0, nullptr, &instance);
+    return instance;
+}
+
+static void CreateCertExtsExecute(napi_env env, void *data)
+{
+    ExtsAsyncContext context = static_cast<ExtsAsyncContext>(data);
+    context->async->errCode = CfCreate(CF_OBJ_TYPE_EXTENSION, context->encodingBlob, &context->extsObj);
+    if (context->async->errCode != CF_SUCCESS) {
+        context->async->errMsg = "create extension failed";
+    }
+}
+
+static void CreateCertExtsComplete(napi_env env, napi_status status, void *data)
+{
+    ExtsAsyncContext context = static_cast<ExtsAsyncContext>(data);
+    if (context->async->errCode != CF_SUCCESS) {
+        ReturnJSResult(env, context->async, nullptr);
+        DeleteExtsAsyncContext(env, context);
+        return;
+    }
+
+    napi_value jsObject = CreateCertExtsJSInstance(env);
+    NapiCertExtension *napiObject = new NapiCertExtension(context->extsObj);
+    napi_wrap(
+        env, jsObject, napiObject,
+        [](napi_env env, void *data, void *hint) {
+            NapiCertExtension *certExts = static_cast<NapiCertExtension *>(data);
+            delete certExts;
+            return;
+        },
+        nullptr, nullptr);
+
+    ReturnJSResult(env, context->async, jsObject);
+    DeleteExtsAsyncContext(env, context);
+}
+
+static napi_value CreateCertExtsAsyncWork(napi_env env, ExtsAsyncContext context)
+{
+    napi_create_async_work(
+        env,
+        nullptr,
+        GetResourceName(env, "CreateCertExtsAsyncWork"),
+        CreateCertExtsExecute,
+        CreateCertExtsComplete,
+        static_cast<void *>(context),
+        &context->async->asyncWork);
+
+    napi_queue_async_work(env, context->async->asyncWork);
+    if (context->async->asyncType == ASYNC_TYPE_PROMISE) {
+        return context->async->promise;
+    } else {
+        return NapiGetNull(env);
+    }
+    return nullptr;
+}
+
+napi_value NapiCreateCertExtension(napi_env env, napi_callback_info info)
+{
+    ExtsAsyncContext context = NewExtsAsyncContext();
+    if (context == nullptr) {
+        CF_LOG_E("Failed to new create exts async context");
+        return nullptr;
+    }
+
+    napi_value result = ParseCreateExtsJSParams(env, info, context);
+    if (result == nullptr) {
+        CF_LOG_E("Failed to parse JS params for create exts object");
+        DeleteExtsAsyncContext(env, context);
+        return nullptr;
+    }
+
+    result = CreateCertExtsAsyncWork(env, context);
+    if (result == nullptr) {
+        CF_LOG_E("Failed to create exts object in async work");
+        DeleteExtsAsyncContext(env, context);
+        return nullptr;
+    }
+
+    return result;
+}
+
+static napi_value NapiCommonOperation(napi_env env, napi_callback_info info, int32_t opType, int32_t typeValue)
+{
+    napi_value jsObject = nullptr;
+    napi_get_cb_info(env, info, nullptr, nullptr, &jsObject, nullptr);
+
+    NapiCertExtension *napiExtsObj = nullptr;
+    napi_unwrap(env, jsObject, reinterpret_cast<void **>(&napiExtsObj));
+    if (napiExtsObj == nullptr) {
+        CF_LOG_E("napi extension objtect is nullptr!");
+        return nullptr;
+    }
+
+    CfObject *extsObj = napiExtsObj->GetObject();
+    if (extsObj == nullptr) {
+        CF_LOG_E("cf objtect is nullptr!");
+        return nullptr;
+    }
+    return CommonOperation(env, info, extsObj, opType, typeValue);
+}
+
+napi_value NapiGetExtensionEncoded(napi_env env, napi_callback_info info)
+{
+    return NapiCommonOperation(env, info, OPERATION_TYPE_GET, CF_GET_TYPE_EXT_ITEM);
+}
+
+static napi_value NapiGetExtensionOidList(napi_env env, napi_callback_info info)
+{
+    return NapiCommonOperation(env, info, OPERATION_TYPE_GET, CF_GET_TYPE_EXT_OIDS);
+}
+
+static napi_value NapiGetExtensionEntry(napi_env env, napi_callback_info info)
+{
+    return NapiCommonOperation(env, info, OPERATION_TYPE_GET, CF_GET_TYPE_EXT_ENTRY);
+}
+
+static napi_value NapiExtensionCheckCA(napi_env env, napi_callback_info info)
+{
+    return NapiCommonOperation(env, info, OPERATION_TYPE_CHECK, CF_CHECK_TYPE_EXT_CA);
+}
+
+static napi_value CertExtsConstructor(napi_env env, napi_callback_info info)
+{
+    napi_value thisVar = nullptr;
+    napi_get_cb_info(env, info, nullptr, nullptr, &thisVar, nullptr);
+    return thisVar;
+}
+
+void NapiCertExtension::DefineCertExtensionJsClass(napi_env env, napi_value exports)
+{
+    napi_property_descriptor desc[] = {
+        DECLARE_NAPI_FUNCTION("createCertExtension", NapiCreateCertExtension),
+    };
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+
+    napi_property_descriptor CertExtensionDesc[] = {
+        DECLARE_NAPI_FUNCTION("getEncoded", NapiGetExtensionEncoded),
+        DECLARE_NAPI_FUNCTION("getOidList", NapiGetExtensionOidList),
+        DECLARE_NAPI_FUNCTION("getEntry", NapiGetExtensionEntry),
+        DECLARE_NAPI_FUNCTION("checkCA", NapiExtensionCheckCA),
+    };
+
+    napi_value constructor = nullptr;
+    napi_define_class(
+        env,
+        "CertExtension",
+        NAPI_AUTO_LENGTH,
+        CertExtsConstructor,
+        nullptr,
+        sizeof(CertExtensionDesc) / sizeof(CertExtensionDesc[0]),
+        CertExtensionDesc,
+        &constructor);
+    napi_create_reference(env, constructor, 1, &classRef_);
+}
+} // namespace CertFramework
+} // namespace OHOS
